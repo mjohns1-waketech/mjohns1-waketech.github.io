9a9647174f769e665a4a0c40a73c59b7d149b3a3
mjohns1-waketech <mjohns1@my.waketech.edu>
2025-03-12 11:33:29 -0400

    Added Gradebook html and js

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+function fetchGradeData(){
+	console.log("Fetching grade data...");
+}
+function populateGradebook(data){
+	console.log("Populating gradebook with data:");
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
