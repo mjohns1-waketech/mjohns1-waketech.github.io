af926c05dd52e709638829b534d8d0b7efc1ee3f
mjohns1-waketech <mjohns1@my.waketech.edu>
2025-04-01 14:32:22 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -12,7 +12,8 @@ function fetchGradeData(){
 		if (xhr.readyState === xhr.DONE){
 			// Check if we're successful
 			if(xhr.status !== 200){
-				console.error('Could not get grades.Status:${xhr.status}');
+				console.error("Could not get grades.Status:${xhr.status}");
+				return;
 			}
 			// And then call the function to update the HTML with our data
 			populateGradebook(JSON.parse(xhr.responseText));
@@ -41,5 +42,5 @@ function populateGradebook(data){
 		});
 	};
 
-const gradeData = fetchGradeData();
+
 populateGradebook(gradeData);
